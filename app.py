3cdccd660c98d74c3fa7801e89921f0379da71b6
Ray <118432471+Rayyr@users.noreply.github.com>
2026-07-23 20:59:02 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# POST Route 
+@app.route("/add", methods=["POST"])
+def add_data():
+    data = request.get_json()
+
+    name = data.get("name")
+    age = data.get("age")
+
+    return jsonify({
+        "message": "Data received:",
+        "name": name,
+        "age": age
+    })
+
+
+# GET Route : Home
+@app.route("/")
+def home():
+    return "<h1>Welcome page</h1>"
+
+# GET Route : Hello
+@app.route("/hello")
+def hello():
+    return "<h2>Hello, User</h2>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
