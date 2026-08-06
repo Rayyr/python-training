99066d536d7effbccf198dc38e7513f567d9fa52
raya <khasaatirayaa@gmail.com>
2026-08-06 15:57:05 +0300

    w8-d2

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,12 @@
 from flask import Flask
-from .extensions import db, login_manager
 from .config import Config
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    db.init_app(app)
-    login_manager.init_app(app)
-
-    from .models.user import User
-
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
-
-    from .routes.auth import auth_bp
-    app.register_blueprint(auth_bp)
+    @app.route("/")
+    def home():
+        return "Flask Deployment Day 2 Running!"
 
     return app
--- app/config.py
+++ app/config.py
@@ -1,4 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Config:
-    SECRET_KEY = "secret"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///students.db"
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
