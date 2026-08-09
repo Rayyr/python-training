989f70e5b8bfc1aa4ee5c677b28914f66f05cc5f
Ray <118432471+Rayyr@users.noreply.github.com>
2026-08-09 13:48:14 +0300

    Refactor user API to include login protection
    Updated user creation to require password and added login protection. Changed JSON response format for user creation and retrieval.

--- app/api/users.py
+++ app/api/users.py
@@ -1,21 +1,32 @@
-from flask import Blueprint, request, jsonify, abort
+from flask import Blueprint, request, jsonify
+from flask_login import login_required
 from ..models.user import User
 from ..extensions import db
 
 api_users_bp = Blueprint('api_users', __name__)
 
+
 @api_users_bp.route('/', methods=['GET'])
 def get_users():
     users = User.query.all()
-    return jsonify([{'id': u.id, 'username': u.username} for u in users])
+    return jsonify([{"id": u.id, "username": u.username} for u in users])
 
+
+# PROTECT USER CREATION
 @api_users_bp.route('/', methods=['POST'])
+@login_required
 def create_user():
-    data = request.json or {}
-    if 'username' not in data or 'password' not in data:
-        abort(400)
-    u = User(username=data['username'])
-    u.set_password(data['password'])
+    data = request.get_json()
+    u = User(username=data['username'], password=data['password'])
     db.session.add(u)
     db.session.commit()
-    return jsonify({'id': u.id, 'username': u.username}), 201
+    return jsonify({"id": u.id}), 201
+
+
+@api_users_bp.route('/<int:id>', methods=['DELETE'])
+@login_required
+def delete_user(id):
+    user = User.query.get_or_404(id)
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({"message": "Deleted"})
